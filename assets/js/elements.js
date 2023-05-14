e1efb11ff1db7b390dc9e732c8b1b7b8638cce7d
Pierre Brito <carlospierre07@gmail.com>
2023-05-14 14:19:49 -0300

    Mapeando elemento em inglês

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -14,4 +14,64 @@ const elements = [
         pt : 'Eu sou um programador voltado para tecnologias WEB e gostaria que você conhecesse mais o meu trabalho nesse site.',
         eng : "I'm a programmer focused on WEB technologies and I would like you to know more about my projects on this site."
     },
+    {
+        id : 'titulo_sobremim',
+        pt : 'Sobre mim',
+        eng : "About me"
+    },
+    {
+        id : 'sobremim_p1',
+        pt : "Me chamo Pierre Brito, vivo em Natal/RN, e estou desenvolvendo esse site que me servirá como portfólio. Meu interesse em programação em 2016 quando comecei a estudar na linguagem Java e, desde então, venho me aprofundando na área.",
+        eng : "My name is Pierre Brito, I live in Natal/RN, and I'm developing this website that will serve as my portfolio. My interest in programming started in 2016 when I started studying the Java language and, since then, I've been delving into the area."
+    },
+    {
+        id : 'sobremim_p2',
+        pt : "Tive o privilégio de participar de alguns projetos que vão ser listados nesse site. Meu principal foco, hoje, está voltado para as tecnologias da linguagem JavasScript.",
+        eng : "I had the privilege of participating in some projects that will be listed on this site. My main focus today is on JavaScript language technologies."
+    },
+    {
+        id : 'titulo_projetos',
+        pt : "Projetos",
+        eng : "Projects"
+    },
+    {
+        id : 'seglocaliza_cargo',
+        pt : "Desenvolvedor <span>@GrupoECOMP</span>",
+        eng : "Developer <span>@GrupoECOMP</span>"
+    },
+    {
+        id : 'seglocaliza_tempo',
+        pt : "Jan 2023 - Mai 2023",
+        eng : "Jan 2023 - May 2023"
+    },
+    {
+        id : 'seglocaliza_descricao',
+        pt : "Solução para visitantes encontrarem a cada em que vão visitar em condomínios horizontais fechado de grande porte.",
+        eng : "Solution for visitors to find where they are going to visit in large closed horizontal condominiums."
+    },
+    {
+        id : 'seglocaliza_t1',
+        pt : "Desenvolvimento: produção do front-end de todas as telas da aplicação.",
+        eng : "Development: front-end production of all application screens."
+    },
+    {
+        id : 'seglocaliza_t2',
+        pt : "Participação na ideia de formulação da solução, bem como seus requisitos e funcionalidades.",
+        eng : "Participation in the idea of ​​formulating the solution, as well as its requirements and functionalities."
+    },
+    {
+        id : 'titulo_aprendizagem',
+        pt : "Projetos de aprendizagem",
+        eng : "Learning's projects"
+    },
+    {
+        id : 'netflix_desc',
+        pt : "Imitando o front-end da Netflix.",
+        eng : "A front-end clone of Netflix."
+    },
 ]
+
+
+ 
+
+

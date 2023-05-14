9863431d25a1d4029c7bcf8e927294475701ac9f
Pierre Brito <carlospierre07@gmail.com>
2023-05-14 13:53:03 -0300

    Botão para selecionar linguagem

--- assets/js/toggleLang.js
+++ assets/js/toggleLang.js
@@ -9,3 +9,19 @@ const toggleLang = (lang) => {
         elements.forEach(e => document.getElementById(e.id).innerHTML = e.eng)
     }
 }
+
+const toggleEng = (e) => {
+    toggleLang(1)
+    document.getElementById('btn_eng').classList.add("active");
+    document.getElementById('btn_pt').classList.remove("active");
+}
+
+const togglePT = (e) => {
+    toggleLang(0)
+    document.getElementById('btn_pt').classList.add("active");
+    document.getElementById('btn_eng').classList.remove("active");
+}
+
+
+document.getElementById('btn_pt').addEventListener("click", togglePT)
+document.getElementById('btn_eng').addEventListener("click", toggleEng)
